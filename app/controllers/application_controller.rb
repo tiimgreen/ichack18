6cf7a471513019a222290aec113827118188e40d
Tim Green <tiimgreen@gmail.com>
2018-01-27 16:14:30 +0000

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_title
+
+  protected
+
+    def set_title
+      unless action_name.to_s.downcase == 'destroy'
+
+        case params[:action]
+        when 'index'
+          @page_title = controller_name.titleize
+        when 'show'
+          @page_title = controller_name.singularize.titleize
+        when 'new', 'create'
+          @page_title = "New #{controller_name.singularize.titleize}"
+        when 'edit', 'update'
+          @page_title = "Edit #{controller_name.singularize.titleize}"
+        else
+          @page_title = action_name.titleize
+        end
+      end
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def page_title(title, base = 'ICHack18')
+    (title.empty? ? base : "#{title} | #{base}").html_safe
+  end
 end
